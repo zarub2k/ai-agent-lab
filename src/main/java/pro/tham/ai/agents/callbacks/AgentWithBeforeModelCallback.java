52f5d6e82b0623b94a6948870f87ca6f35ddb46f
Tham <zarub2k@gmail.com>
2025-07-02 00:17:03 +0530

    small change is done

--- src/main/java/pro/tham/ai/agents/callbacks/AgentWithBeforeModelCallback.java
+++ src/main/java/pro/tham/ai/agents/callbacks/AgentWithBeforeModelCallback.java
@@ -1,6 +1,10 @@
 package pro.tham.ai.agents.callbacks;
 
+import com.google.adk.agents.CallbackContext;
+import com.google.adk.agents.Callbacks;
 import com.google.adk.agents.LlmAgent;
+import com.google.adk.models.LlmRequest;
+import java.util.Optional;
 import pro.tham.ai.agents.base.AgentExecutor;
 
 /**
@@ -14,13 +18,21 @@ public class AgentWithBeforeModelCallback {
     public static void main(String[] args) {
         System.out.println("*** Agent with before model callback ***");
         
+        Callbacks.BeforeModelCallbackSync beforeModelCallback = 
+                (CallbackContext callbackContext, LlmRequest llmRequest) -> {
+                    System.out.println("Callback is called before model....: " + callbackContext.agentName());
+                    return Optional.empty();
+                };
+        
         LlmAgent llmAgent = LlmAgent.builder()
                 .name(NAME)
                 .model(MODEL)
                 .description("Personal agent to answer questions about the country and its capitals")
                 .instruction("You are a helful agent")
+                .beforeModelCallbackSync(beforeModelCallback)
                 .build();
         
+        System.out.println("Agent is about to be called");
         AgentExecutor.execute(llmAgent, NAME, MODEL);
     }
 }
