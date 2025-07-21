786d66153e1361a64426511861c47f09300e2845
Tham <zarub2k@gmail.com>
2025-07-22 00:05:59 +0530

    Callback is changed

--- src/main/java/pro/tham/ai/agents/callbacks/AiCallbacks.java
+++ src/main/java/pro/tham/ai/agents/callbacks/AiCallbacks.java
@@ -2,6 +2,8 @@ package pro.tham.ai.agents.callbacks;
 
 import com.google.adk.agents.CallbackContext;
 import com.google.adk.agents.Callbacks;
+import com.google.adk.models.LlmRequest;
+import com.google.adk.models.LlmResponse;
 import com.google.adk.sessions.State;
 import com.google.genai.types.Content;
 import com.google.genai.types.Part;
@@ -46,10 +48,43 @@ public final class AiCallbacks {
                 return Optional.empty();
             };
     
+    
+    public static Callbacks.BeforeModelCallbackSync beforeModel
+            = (CallbackContext callbackContext, LlmRequest llmRequest) -> {
+                System.out.println("Callback before model: " + callbackContext.agentName());
+                
+                String userMessage = "";
+                List<Content> contents = llmRequest.contents();
+                if (contents != null || !contents.isEmpty()) {
+                    userMessage = contents.getLast().parts().get().get(0).text().orElse("");
+                }
+                
+                System.out.println("Inspecting last user message: " + userMessage);
+                if (userMessage.toUpperCase().contains("ISS")) {
+                    return Optional.of(
+                            buildResponse("model", "LLM call is blocked by the before model callback"));
+                }
+                
+                return Optional.empty();
+            };
+
+    public static Callbacks.AfterModelCallbackSync afterModel
+            = (CallbackContext callbackContext, LlmResponse llmResponse) -> {
+                System.out.println("Callback after model: " + callbackContext.agentName());
+                return Optional.empty();
+            };
+    
     public static Content buildContent(String role, String value) {
         return Content.builder()
                 .parts(List.of(Part.fromText(value)))
                 .role(role)
+                .build();
+    }
+    
+    static LlmResponse buildResponse(String role, String value) {
+        return LlmResponse
+                .builder()
+                .content(buildContent(role, value))
                 .build();
     }
 }
